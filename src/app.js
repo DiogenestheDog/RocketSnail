a492b24a3eec9aadd4b66751e8acd96615668783
Chloe Guthrie <rguthrie14@gmail.com>
2021-05-13 16:12:15 -0700

    fps test

--- src/app.js
+++ src/app.js
@@ -109,7 +109,12 @@ window.addEventListener('DOMContentLoaded', () => {
     const gameLoop = (timestamp) => {
 
         // look into throttling this
-
+        secondsPassed = (timestamp - oldTimeStamp) / 1000;
+        oldTimeStamp = timestamp;
+    
+        // Calculate fps
+        fps = Math.round(1 / secondsPassed);
+        console.log(fps)
             erase();
             moveSprite(rocketSnail, tracker);
             window.requestAnimationFrame(gameLoop);
